853e51fe5ac215cd3d9098b4c7e26a070e1727ea
WangJie <765139628@qq.com>
2017-03-12 23:27:40 +0800

    学习jquery第二天

--- doc5.js
+++ doc5.js
@@ -13,7 +13,17 @@
             if (!selector) {
                 return this;
             }
-
+            if (typeof selector == 'object') {//如果是 selector个对象
+                var selector = [selector];//把对象放入数组中
+                for (var i = 0; i < selector.length; i++) {
+                    this[i] = selector[i];//把数组中的每个对象放入当前jquery对象中
+                }
+                this.length = selector.length;
+                return this;//返回数组
+            } else if (typeof selector == 'function') {//如果传入的是个函数
+                Kodo.ready(selector);//调用ready方法
+                return;
+            }
             var selector = selector.trim();
             var elm;
 
@@ -84,7 +94,8 @@
             a.selector = parent.tagName.toLocaleLowerCase();//selector属性为父节点的标签名称
             a.length = 1;//设置数组长度为1
             return a;
-        }, parents: function () {
+        },
+        parents: function () {
             var a = Kodo(),
                 i = 0;
             while ((this[0] = this[0]['parentNode']) && this[0].nodeType !== 9) {//如果父节点存在并且父节点不是document树
@@ -96,17 +107,51 @@
             }
             a.length = i;//给我们的jquery对象赋值长度属性
             return a;
-        }
+        },
     };
     Kodo.prototype.init.prototype = Kodo.prototype;//即init方法生成的对象可以调用Kodo里的所有方法
     Kodo.ajax = function () {
         console.log(this);
     };
-    window.$ = Kodo;
+    Kodo.ready = function (fn) {
+
+        doc.addEventListener('DOMContentLoaded', fn, false);//在事件冒泡阶段触发
+        doc.removeEventListener('DOMContentLoaded', fn, true);//事件捕获阶段移除
+
+    };
+    Kodo.each = function (obj, callback) {
+        var len = obj.length,
+            constru = obj.constructor,
+            i = 0;
+
+        if (constru === window.$) {//判断该对象的构造器是否是Kodo函数
+            for (; i < len; i++) {
+                var val = callback.call(obj[i], i, obj[i]);//如果是Kodo函数 则让回调方法的执行域指向该对象（该对象为一个数组:Kodo数组）
+                if (val === false) break;//如果返回调回值为空 则跳出循环
+            }                           //！！！！！call方法的第一个参数是改变方法中的this指向后面方法都是传入的参数
+                                        //apply方法第一个参数也是改变this的指针 第二个方法是一个list可以传入参数为数组形态
+        } else if (isArray(obj)) {//如果是数组
+            for (; i < len; i++) {
+                var val = callback.call(obj[i], i, obj[i]);//同上
+                if (val === false) break;
+            }
+        } else {
+            for (i in obj) {//如果是对象 则for in遍历出每个属性给回调方法遍历
+                var val = callback.call(obj[i], i, obj[i]);
+                if (val === false) break;
+            }
+        }
+
+    };
+    window.$ = Kodo;//在window的$上挂载Kodo方法以便可以调用
 
     function sibling(cur, dir) {
         while ((cur = cur[dir]) && cur.nodeType !== 1) {
         } //当cur不为空并且不nodetype!=1（即不是DOM元素）的时候什么都不做
         return cur;//返回该子元素
+    }
+
+    function isArray(obj) {
+        return Array.isArray(obj);
     }
 })(window, document);
