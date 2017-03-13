df2eb46e4494459e8ed5237ac26916d26971f0d1
WJ <765139628@qq.com>
2017-03-13 17:50:37 +0800

    第三天

--- myQuery1.0.js
+++ myQuery1.0.js
@@ -9,6 +9,12 @@
         length: 0,
         splice: new Array().splice,//相当于掉此处调用了数组的splice方法可以把kodo对象当作数组进行splice操作但是不会切割掉原型属性
         selector: '',
+        extends: function () {//浅拷贝
+            var options = arguments[0];
+            for (var key in options) {
+                this[key] = options[key]
+            }
+        },
         init: function (selector) {
             if (!selector) {
                 return this;
@@ -181,7 +187,175 @@
                 return arr;
             }
             return this;
+        },
+        before: function (str) {
+            for (var i = 0; i < this.length; i++) {
+                doAppend(this[i], 'beforeBegin', str);
+            }
+            return this;
+        },
+        append: function (str) {
+            for (var i = 0; i < this.length; i++) {
+                doAppend(this[i], 'beforeend', str);
+            }
+            return this;
+        },
+        after: function (str) {
+            for (var i = 0; i < this.length; i++) {
+                domAppend(this[i], 'afterEnd', str);
+            }
+            return this;
+        },
+        remove: function () {
+            for (var i = 0; i < this.length; i++) {
+                this[i].parentNode.removeChild(this[i]);
+            }
+            return this;
+        }, html: function (value) {
+            var arr = [];
+            if (value === undefined && this[0].nodeType === 1) {
+                for (var i = 0; i < this.length; i++) {
+                    arr.push(this[0].innerHTML);
+                }
+                return arr;
+            } else {
+                for (var i = 0; i < this.length; i++) {
+                    var node = this[i];
+                    if (node.children.length > 0) {
+                        for (var c = 0; c < node.children.length; c++) {
+                            node.removeChild(node.children[c]);//防止事件引用导致内存泄漏
+                        }
+                    }
+                    node.innerHTML = value;
+                }
+            }
+            return this;
+        }, text: function (val) {
+            var arr = [];
+            if (val === undefined && this[0].nodeType === 1) {
+                for (var i = 0; i < this.length; i++) {
+                    arr.unshift(this[0].innerText);
+                }
+                return arr;
+            } else {
+                for (var i = 0; i < this.length; i++) {
+                    this[i].innerText = val;    //以文本形式插入dom元素
+                }
+            }
+        }, on: function (type, selector, fn) {
+            if (typeof selector == 'function') {
+                fn = selector; //两个参数的情况
+                //事件绑定过程
+            } else {
+                //事件委托过程
+                for (var i = 0; i < this.length; i++) {
+                    if (!this[i].deleId) {
+                        this[i].deleId = ++Kodo.deleId;
+                        //同样是判断是否有唯一id
+
+                        Kodo.deleEvents[Kodo.deleId] = {};
+                        //没有则创建id对象 也就是f.deleEvents[]新开辟一个新对象
+
+                        Kodo.deleEvents[Kodo.deleId][selector] = {};
+
+                        //构造 selector对象
+                        /*
+                         *  如 Kodo.deleEvents[1] =
+                         *  ｛
+                         *       "#box li" : {},
+                         *        "#pox" : {}
+                         *   ｝
+                         */
+
+                        Kodo.deleEvents[Kodo.deleId][selector][type] = [fn];
+                        //构造我们的事件数组
+                        /*
+                         *  如 Kodo.deleEvents["#box li"] =
+                         *  {
+                         *       "click" : [fn1,fn2...],
+                         *        "touchstart" : [fn1,fn2....]
+                         *   }
+                         */
+                        delegate(this[i], type, selector);
+                        //用委托的方式进行绑定
+                    } else {
+                        //如果id存在的情况
+                        var id = this[i].deleId,
+                            position = Kodo.deleEvents[id];//委托元素的事件存储位置
+
+                        if (!position[selector]) {
+                            //先判断如果selector存储的对象不存在
+                            position[selector] = {};
+                            //新建selector对象 (与上面的selector构造相同)
+                            position[selector][type] = [fn];
+                            //构造事件数组对象   (与上面的type构造相同)
+
+                            delegate(this[i], type, selector);
+                            //因为是新的selector 所以要再绑定
+                        } else {
+                            //selector 存储对象存在的情况
+                            if (position[selector][type]) {
+                                //如果事件数组已经有了，则直接push进来
+                                position[selector][type].push(fn);
+
+                            } else {
+                                //如果事件数组没有，那就构造事件数组
+                                position[selector][type] = [fn];
+
+                                //因为是新的绑定的事件，所以要重新绑定
+                                delegate(this[i], type, selector);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }, off: function (type, selector) {
+            if (arguments.length == 0) {
+                //如果没传参数，清空所有事件
+
+            } else if (arguments.length == 1) {
+                //指定一个参数，则清空对应的事件
+
+            } else {
+                //直接根据dom上存有的deleId，找到对应的deleEvents里的位置
+                //删除委托元素上的type事件数组即可
+                for (var i = 0; i < this.length; i++) {
+                    var id = this[i].deleId;
+                    delete Kodo.deleEvents[id][selector][type];
+                }
+            }
+        }, width: function (w) {
+            if (arguments.length == 1) {
+                for (var i = 0; i < this.length; i++) {
+                    this[i].style.width = w + 'px';
+                }
+            } else {
+                if (this[0].document === doc) {//如果是document 和window
+                    return this[0].innerWidth;  //返回内容的宽度
+                } else if (this[0].nodeType === 9) {
+                    return document.documentElement.clientWidth;
+                } else {
+                    return parseInt(getComputedStyle(this[0], null)['width']);
+                }
+            }
+        }, height: function (h) {
+            if (arguments.length == 1) {
+                for (var i = 0; i < this.length; i++) {
+                    this[i].style.height = h + 'px';
+                }
+            } else {
+                if (this[0].document === doc) {
+                    return this[0].innerHeight;
+                } else if (this[0].nodeType === 9) {
+                    return document.documentElement.clientHeight;
+                } else {
+                    return parseInt(getComputedStyle(this[0], null)['height']);
+                }
+            }
         }
+
+
     };
     Kodo.prototype.init.prototype = Kodo.prototype;//即init方法生成的对象可以调用Kodo里的所有方法
     Kodo.get = function (url, sucBack, complete) {//添加get静态方法
@@ -232,7 +406,11 @@
         }
 
     };
-    window.$ = Kodo;//在window的$上挂载Kodo方法以便可以调用
+    Kodo.deleEvents = []; //事件委托存放的事件
+    Kodo.deleId = 0; //事件委托的唯一标识
+    Kodo.extends = Kodo.prototype.extends;
+
+    window.$ = Kodo;//在window的$上挂载Kodo构造器方法以便可以调用
 
     function sibling(cur, dir) {
         while ((cur = cur[dir]) && cur.nodeType !== 1) {
@@ -295,5 +473,63 @@
         function ajaxComplete(status) {
             options.complete && options.complete(status);
         }
+    }
+
+    function delegate(agent, type, selector) {
+        // agent.addEventListener(type, function (e) {//给元素绑定指定事件
+        //
+        //     var target = e.target;//触发事件的元素
+        //     var ctarget = e.currentTarget;//当前执行事件的元素
+        //     var bubble = true;//是否冒泡
+        //
+        //     while (bubble && target != ctarget) {//当冒泡并且当前执行事件的元素不是触发事件的元素是：即冒泡触发事件的元素
+        //         if (filiter(agent, selector, target)) {//判断该触发事件的元素是否是绑定元素的”selector“元素
+        //             bubble = fn.call(target, e);//如果是指定的子元素触发的事件 则调用回调方法处理事件 并把执行域指向触发事件的元素
+        //             return bubble;
+        //         }
+        //         target = target.parentNode;//把target赋值为它的父级元素  继续循环判断 是否触发冒泡事件 一直到冒泡结束
+        //     }
+        // }, false);
+        var id = agent.deleId; //先获取被委托元素的deleId
+        agent.addEventListener(type, function (e) {
+            var target = e.target;
+            var ctarget = e.currentTarget;
+            var bubble = true;
+
+            while (bubble && target != ctarget) {
+                if (filiter(agent, selector, target)) {
+                    for (var i = 0; i < Kodo.deleEvents[id][selector][type].length; i++) {
+                        bubble = Kodo.deleEvents[id][selector][type][i].call(target, e);
+                        //循环事件数组 直接call
+                    }
+                }
+                target = target.parentNode;
+                return bubble;
+            }
+        }, false);
+        //总结：每次触发事件时 从最底层往上遍历所有节点 查看有没有符合条件的那个节点
+        //如果触发事件的元素是 绑定了事件的子元素 中的某一个
+        //就触发回调方法 并且把回调的方法的执行域指向触发事件的那个元素
+        //如果不是就继续往上找
+        //agent相当于一个事件委托人
+        //举个例子： <body><agent><a></a></agent></body>
+        //当a触发了事件时 会向上冒泡 于是agent也会被执行事件 此时 agent中的 target是a currentTarget是agent
+        //判断如果target!=currentTarget就是为了表示该事件是由它的子元素触发的 而不是它
+        //然后判断是否是指定的那个子元素触发的  不是的话就向a的上面找元素  比对触发事件的父级元素中有没有匹配的指定元素
+        //没有的话一直向上找  直到agent为止 不会向父级元素冒泡 找到后调用回调方法
+        //也就是说 指定元素的子元素触发事件 指定元素被冒泡触发的事件 也会算
+
+        function filiter(agent, selector, target) {//过滤判断元素是否为指定元素
+            var nodes = agent.querySelectorAll(selector);//查询到指定元素
+            for (var i = 0; i < nodes.length; i++) {//遍历指定元素 是否有与target相等的
+                if (nodes[i] == target) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    function doAppend(elm, type, str) {
+        elm.insertAdjacentHTML(type, str);
     }
 })(window, document);
